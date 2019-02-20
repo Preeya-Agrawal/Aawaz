70c9e04544be92105fe175e3888342c34db718c5
unknown <Priya Agrawal>
2019-02-20 17:00:41 +0545

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use auth;
+use Session;
+class AdminController extends Controller
+{
+    public function index(Request $request){
+        if ($request->isMethod('post')){
+            $data=$request->input();
+            if(Auth::attempt(['email'=> $data['email'],'password'=>$data['password'],'admin'=>'1'])){
+                // echo "success";die;
+                // Session::put('adminSession',$data['email']);
+                return redirect ('/admin/dashboard');
+               
+             }
+              else
+              
+              { return redirect ('/admin')->with('flash_message_error','invalid password or email');
+            }
+        }       
+         return view('admin.login');
+    }
+    public function dashboard(){
+        // if(Session::has('adminSession')){
+        //     //perform all dashboard tasks
+    
+        //  } else{
+        // return redirect ('/admin')->with('flash_message_error','Please login to access');  
+        // }
+        return view ('admin.dashboard');
+    }
+    public function logout(){
+       Session::flush();
+       return redirect ('/admin')->with('flash_message_success','Logout successfully');
+    }
+    public function settings(){
+        return view ('admin.settings');
+    }
+
+}
